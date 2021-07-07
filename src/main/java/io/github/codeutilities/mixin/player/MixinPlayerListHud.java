08a2f0ad3182c539b03a46f162d55ee5ee822812
tk2217 <52385653+tk2217@users.noreply.github.com>
2021-07-07 10:56:46 -0500

    Hopefully fixed crashes with the tab list 2: Electric Boogaloo cleanup edition.

--- src/main/java/io/github/codeutilities/mixin/player/MixinPlayerListHud.java
+++ src/main/java/io/github/codeutilities/mixin/player/MixinPlayerListHud.java
@@ -3,20 +3,19 @@ package io.github.codeutilities.mixin.player;
 import io.github.codeutilities.config.Config;
 import io.github.codeutilities.features.social.tab.CodeUtilitiesServer;
 import io.github.codeutilities.features.social.tab.User;
-import java.util.UUID;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.scoreboard.Team;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.UUID;
 
 @Mixin(PlayerListHud.class)
 public abstract class MixinPlayerListHud {
