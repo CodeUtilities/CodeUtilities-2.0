32043bd5e3a6850c08503b75b7acd70a4c823817
tk2217 <52385653+tk2217@users.noreply.github.com>
2021-07-07 13:12:19 -0500

    Updated spawn/dev detection to 1.17

--- src/main/java/io/github/codeutilities/mixin/item/MixinItemSlotUpdate.java
+++ src/main/java/io/github/codeutilities/mixin/item/MixinItemSlotUpdate.java
@@ -43,75 +43,75 @@ public class MixinItemSlotUpdate {
             }
 
             //TODO: figure out what NbtList.toText() is in 1.17 and update it
-//            if (DFInfo.isOnDF() && stack.getName().getString().contains("◇ Game Menu ◇")
-//                    && lore.toText().getString().contains("\"Click to open the Game Menu.\"")
-//                    && lore.toText().getString().contains("\"Hold and type in chat to search.\"")) {
-//
-//                if (DFInfo.currentState.getMode() != State.Mode.SPAWN) {
-//                    DFInfo.currentState.sendLocate();
-//
-//                    // Auto fly
-//                    if (Config.getBoolean("autofly")) {
-//                        if (System.currentTimeMillis() > lobbyTime) { // theres a bug with /fly running twice this is a temp fix.
-//                            mc.player.sendChatMessage("/fly");
-//                            ReceiveChatMessageEvent.cancelFlyMsg = true;
-//                            lobbyTime = System.currentTimeMillis() + 1000;
-//                        }
-//
-//                    }
-//
-//                    CPU_UsageText.lagSlayerEnabled = false;
-//
-//                    // fs toggle
-//                    FlightspeedToggle.fs_is_normal = true;
-//                }
-//            }
-//
-//            if (DFInfo.isOnDF() && mc.player.isCreative() && stack.getName().getString().contains("Player Event")
-//                    && lore.toText().getString().contains("\"Used to execute code when something\"")
-//                    && lore.toText().getString().contains("\"is done by (or happens to) a player.\"")
-//                    && lore.toText().getString().contains("\"Example:\"")) {
-//
-//                if (DFInfo.currentState.getMode() != State.Mode.DEV) {
-//                    DFInfo.currentState.sendLocate();
-//                    DFInfo.plotCorner = mc.player.getPos().add(10, -50, -10);
-//
-//                    // Auto LagSlayer
-//                    if (!CPU_UsageText.lagSlayerEnabled && Config.getBoolean("autolagslayer")) {
-//                        mc.player.sendChatMessage("/lagslayer");
-//                        ReceiveChatMessageEvent.cancelLagSlayerMsg = true;
-//                    }
-//
-//                    // fs toggle
-//                    FlightspeedToggle.fs_is_normal = true;
-//
-//                    long time = System.currentTimeMillis() / 1000L;
-//                    if (time - lastDevCheck > 1) {
-//
-//                        new Thread(() -> {
-//                            try {
-//                                Thread.sleep(10);
-//                                if (Config.getBoolean("autoRC")) {
-//                                    mc.player.sendChatMessage("/rc");
-//                                }
-//                                if (Config.getBoolean("autotime")) {
-//                                    mc.player.sendChatMessage("/time " + Config.getLong("autotimeval"));
-//                                    ReceiveChatMessageEvent.cancelTimeMsg = true;
-//                                }
-//                                if (Config.getBoolean("autonightvis")) {
-//                                    mc.player.sendChatMessage("/nightvis");
-//                                    ReceiveChatMessageEvent.cancelNVisionMsg = true;
-//                                }
-//                            } catch (Exception e) {
-//                                CodeUtilities.log(Level.ERROR, "Error while executing the task!");
-//                                e.printStackTrace();
-//                            }
-//                        }).start();
-//
-//                        lastDevCheck = time;
-//                    }
-//                }
-//            }
+            if (DFInfo.isOnDF() && stack.getName().getString().contains("◇ Game Menu ◇")
+                    && lore.toString().contains("\"Click to open the Game Menu.\"")
+                    && lore.toString().contains("\"Hold and type in chat to search.\"")) {
+
+                if (DFInfo.currentState.getMode() != State.Mode.SPAWN) {
+                    DFInfo.currentState.sendLocate();
+
+                    // Auto fly
+                    if (Config.getBoolean("autofly")) {
+                        if (System.currentTimeMillis() > lobbyTime) { // theres a bug with /fly running twice this is a temp fix.
+                            mc.player.sendChatMessage("/fly");
+                            ReceiveChatMessageEvent.cancelFlyMsg = true;
+                            lobbyTime = System.currentTimeMillis() + 1000;
+                        }
+
+                    }
+
+                    CPU_UsageText.lagSlayerEnabled = false;
+
+                    // fs toggle
+                    FlightspeedToggle.fs_is_normal = true;
+                }
+            }
+
+            if (DFInfo.isOnDF() && mc.player.isCreative() && stack.getName().getString().contains("Player Event")
+                    && lore.toString().contains("\"Used to execute code when something\"")
+                    && lore.toString().contains("\"is done by (or happens to) a player.\"")
+                    && lore.toString().contains("\"Example:\"")) {
+
+                if (DFInfo.currentState.getMode() != State.Mode.DEV) {
+                    DFInfo.currentState.sendLocate();
+                    DFInfo.plotCorner = mc.player.getPos().add(10, -50, -10);
+
+                    // Auto LagSlayer
+                    if (!CPU_UsageText.lagSlayerEnabled && Config.getBoolean("autolagslayer")) {
+                        mc.player.sendChatMessage("/lagslayer");
+                        ReceiveChatMessageEvent.cancelLagSlayerMsg = true;
+                    }
+
+                    // fs toggle
+                    FlightspeedToggle.fs_is_normal = true;
+
+                    long time = System.currentTimeMillis() / 1000L;
+                    if (time - lastDevCheck > 1) {
+
+                        new Thread(() -> {
+                            try {
+                                Thread.sleep(10);
+                                if (Config.getBoolean("autoRC")) {
+                                    mc.player.sendChatMessage("/rc");
+                                }
+                                if (Config.getBoolean("autotime")) {
+                                    mc.player.sendChatMessage("/time " + Config.getLong("autotimeval"));
+                                    ReceiveChatMessageEvent.cancelTimeMsg = true;
+                                }
+                                if (Config.getBoolean("autonightvis")) {
+                                    mc.player.sendChatMessage("/nightvis");
+                                    ReceiveChatMessageEvent.cancelNVisionMsg = true;
+                                }
+                            } catch (Exception e) {
+                                CodeUtilities.log(Level.ERROR, "Error while executing the task!");
+                                e.printStackTrace();
+                            }
+                        }).start();
+
+                        lastDevCheck = time;
+                    }
+                }
+            }
         }
     }
 
