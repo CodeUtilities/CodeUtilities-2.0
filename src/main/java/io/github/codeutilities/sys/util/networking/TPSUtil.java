fb78a35d16e8e350ce03eef811348b4389320398
RyanLandDev <ryanlandofficial@hotmail.com>
2021-07-22 13:24:05 +0200

    Large refactors + Removed CountCommand

---
+++ src/main/java/io/github/codeutilities/sys/util/networking/TPSUtil.java
@@ -0,0 +1,33 @@
+package io.github.codeutilities.sys.util.networking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TPSUtil {
+
+    private static long lastTpsTimestamp = 0;
+    private static List<Long> measurements = new ArrayList<>();
+
+    public static void addTick() {
+        synchronized (measurements) {
+            measurements.add(System.currentTimeMillis());
+
+            if (measurements.size() > 15) measurements.remove(0);
+        }
+    }
+
+    public static void reset() {
+        synchronized (measurements) {
+            measurements.clear();
+        }
+    }
+
+    public static double calculateTps() {
+        long current = measurements.get(measurements.size() - 1);
+        long previous = measurements.get(0);
+
+        double secDiff = Math.max((current - previous) / (1000.0 * (measurements.size() - 1)), 1.0);
+
+        return 20.0f / secDiff;
+    }
+}
