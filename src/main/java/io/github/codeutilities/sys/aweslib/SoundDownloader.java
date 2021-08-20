8ed33aede16013bfdfe8634ed5bab5e6bcd056f9
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-08-20 17:32:42 +0300

    Aweslib 1/2

---
+++ src/main/java/io/github/codeutilities/sys/aweslib/SoundDownloader.java
@@ -0,0 +1,53 @@
+package io.github.codeutilities.sys.aweslib;
+
+import io.github.codeutilities.CodeUtilities;
+import io.github.codeutilities.sys.player.chat.ChatUtil;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+
+public class SoundDownloader {
+
+    // This is a SYSTEM function it is triggered by consented().
+    public static File download(String URL, String ToLocation) {
+        try {
+            FileUtils.copyURLToFile(new URL(URL), new File(ToLocation), 10000, 10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // When a user consents to a sound (OR it was in the consent list) This is triggered.
+    public static void consented(String sound) {
+        // getDirectory
+        Path dir = FabricLoader.getInstance().getGameDir().resolve("CodeUtilities").resolve("aweslib");
+        // Check if the plot reaches the maxMB limit. It is possible for plots to kind of bypass this.
+        if(awefile.bytesToMeg(awefile.getFolderSize(dir.toFile())) > awemanager.maxMb) {
+            ChatUtil.sendMessage("< aweslib > The plot "+ awemanager.plotID + " reaches the maxMb limit of " + awemanager.maxMb);
+        }
+        // Check if the files in the folder is less htan maxAmnt
+        java.io.File[] fileAmount = dir.toFile().listFiles();
+        if(fileAmount.length > awemanager.maxAmnt) {
+            ChatUtil.sendMessage("< aweslib > The plot "+ awemanager.plotID + " reaches the maxAmnt limit of " + awemanager.maxAmnt);
+        }
+        // Does both checks.
+        if(awefile.bytesToMeg(awefile.getFolderSize(dir.toFile())) < awemanager.maxMb && fileAmount.length < awemanager.maxAmnt) {
+            // Makes sure the name for the file is legal.
+            String name = sound.replace("/", "").replace(":", "").replace(".", "") + ".wav";
+            String loc = dir.toString() + "/" + name; // Setting location.
+            // Download fle:
+            File sFile = download(sound, loc);
+        }
+        awemanager.DownloadedIndex++;
+        int needed = awemanager.sounds.size();;
+        if(awemanager.DownloadedIndex == needed) {
+            ChatUtil.sendMessage("< aweslib > All sounds of the plot " + awemanager.plotID + " have been downloaded!");
+            awemanager.DownloadPhase = false;
+        }
+    }
+
+}
