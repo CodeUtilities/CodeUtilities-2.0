c20168d3ecbae8f40f5080957cdcc2cb76ab43ab
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-08-21 08:54:48 +0300

    Fixed up Command.

--- src/main/java/io/github/codeutilities/mod/commands/impl/other/AWESLibCommand.java
+++ src/main/java/io/github/codeutilities/mod/commands/impl/other/AWESLibCommand.java
@@ -3,6 +3,7 @@ package io.github.codeutilities.mod.commands.impl.other;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import io.github.codeutilities.mod.commands.Command;
 import io.github.codeutilities.mod.commands.arguments.ArgBuilder;
 import io.github.codeutilities.mod.commands.arguments.types.FileArgumentType;
@@ -26,59 +27,50 @@ import java.io.File;
 
 public class AWESLibCommand extends Command {
 
+    private static int run2(CommandContext<FabricClientCommandSource> ctx) {
+        if (AWEManager.downloadPhase) {
+            AWEUtils.sendMessage("Domain Blocked, to report this Plot send a message to SirObby_ or (Sir Obsidian#2640).");
+            AWEManager.downloadedIndex++;
+            if (AWEManager.sounds.size() == AWEManager.downloadedIndex) {
+                AWEUtils.sendMessage("All sounds of the plot " + AWEManager.plotID + " have been downloaded!");
+                AWEManager.downloadPhase = false;
+            }
+        } else {
+            AWEUtils.sendMessage("There is no need for an /aweslib action.");
+        }
+        return 1;
+    }
+
+    private static int run3(CommandContext<FabricClientCommandSource> ctx) {
+        if (AWEManager.downloadPhase) {
+            AWEUtils.sendMessage("You have decided to allow this domain.");
+            AWEUtils.sendMessage("If you want this domain to always be allowed go to the settings.");
+            new Thread(() -> {
+                SoundDownloader.consented(AWEManager.pubSound);
+            }).start();
+        } else {
+            AWEUtils.sendMessage("There is no need for an /aweslib action.");
+        }
+        return 1;
+    }
+
     @Override
     public void register(MinecraftClient mc, CommandDispatcher<FabricClientCommandSource> cd) {
         cd.register(ArgBuilder.literal("aweslib")
                 .then(ArgBuilder.literal("allow")
-                        .executes(ctx -> {
-                            if(AWEManager.downloadPhase) {
-                                AWEUtils.sendMessage("You have decided to allow this domain.");
-                                AWEUtils.sendMessage("If you want this domain to always be allowed go to the settings.");
-                                new Thread(() -> {
-                                    SoundDownloader.consented(AWEManager.pubSound);
-                                }).start();
-                            } else {
-                                AWEUtils.sendMessage("There is no need for an /aweslib action.");
-                            }
-                                    return 1;
-                                })
-
+                        .executes(AWESLibCommand::run3)
                 )
                 .then(ArgBuilder.literal("block")
-                        .executes(ctx -> {
-                            if(AWEManager.downloadPhase) {
-                                AWEUtils.sendMessage("You have decided to block this domain. (not saved)");
-                                AWEManager.downloadedIndex++;
-                                if(AWEManager.sounds.size() == AWEManager.downloadedIndex) {
-                                    AWEUtils.sendMessage("All sounds of the plot " + AWEManager.plotID + " have been downloaded!");
-                                    AWEManager.downloadPhase = false;
-                                }
-                            } else {
-                                AWEUtils.sendMessage("There is no need for an /aweslib action.");
-                            }
-                            return 1;
-                        })
+                        .executes(this::run)
                 ).then(ArgBuilder.literal("report")
-                        .executes(ctx -> {
-                            if(AWEManager.downloadPhase) {
-                                AWEUtils.sendMessage("Domain Blocked, to report this Plot send a message to SirObby_ or (Sir Obsidian#2640).");
-                                AWEManager.downloadedIndex++;
-                                if(AWEManager.sounds.size() == AWEManager.downloadedIndex) {
-                                    AWEUtils.sendMessage("All sounds of the plot " + AWEManager.plotID + " have been downloaded!");
-                                    AWEManager.downloadPhase = false;
-                                }
-                            } else {
-                                AWEUtils.sendMessage("There is no need for an /aweslib action.");
-                            }
-                            return 1;
-                        })
+                        .executes(AWESLibCommand::run2)
                 )
         );
     }
 
     @Override
     public String getDescription() {
-        return "/aweslib allow\n/aweslib block\n/aweslib report\n\nIt's aweslib command no explanations needed.";
+        return "/aweslib allow\n/aweslib block\n/aweslib report\n\nIts for what action to do for a domain when downloading..";
     }
 
     @Override
@@ -86,4 +78,17 @@ public class AWESLibCommand extends Command {
         return "/aweslib";
     }
 
+    private int run(CommandContext<FabricClientCommandSource> ctx) {
+        if (AWEManager.downloadPhase) {
+            AWEUtils.sendMessage("You have decided to block this domain. (not saved)");
+            AWEManager.downloadedIndex++;
+            if (AWEManager.sounds.size() == AWEManager.downloadedIndex) {
+                AWEUtils.sendMessage("All sounds of the plot " + AWEManager.plotID + " have been downloaded!");
+                AWEManager.downloadPhase = false;
+            }
+        } else {
+            AWEUtils.sendMessage("There is no need for an /aweslib action.");
+        }
+        return 1;
+    }
 }
