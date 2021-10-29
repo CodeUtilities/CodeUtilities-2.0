632ef956a1cfa8ed65cb22622dd1ee1112323b61
BlazeMCworld <blazemcworld@users.noreply.github.com>
2021-10-29 13:49:56 +0200

    updated /itemdata to 1.17

--- src/main/java/io/github/codeutilities/mod/commands/impl/item/ItemdataCommand.java
+++ src/main/java/io/github/codeutilities/mod/commands/impl/item/ItemdataCommand.java
@@ -9,55 +9,56 @@ import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.visitor.NbtTextFormatter;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.ClickEvent.Action;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.NbtText;
 import net.minecraft.util.registry.Registry;
 
 public class ItemdataCommand extends Command {
 
     @Override
     public void register(MinecraftClient mc, CommandDispatcher<FabricClientCommandSource> cd) {
-        //TODO: update to 1.17
-//        cd.register(ArgBuilder.literal("itemdata")
-//                .executes(ctx -> {
-//                    ItemStack item = mc.player.getMainHandStack();
-//                    NbtCompound nbt = item.getNbt();
-//                    if (nbt != null) {
-//                        ChatUtil.sendMessage(String.format("§5----------§dItem Data for %s§5----------", item.getName().getString()));
-//                        mc.player.sendMessage(nbt.toText("  ", 0), false);
-//
-//
-//                        String formatted = nbt.toText("  ", 0).getString();
-//                        String unformatted = nbt.toString();
-//
-//                        LiteralText msg1 = new LiteralText("§5Click here to copy a ");
-//                        LiteralText msg2 = new LiteralText("§d§lFormatted§5, ");
-//                        LiteralText msg3 = new LiteralText("§d§lUnformatted");
-//                        LiteralText msg4 = new LiteralText("§5 or ");
-//                        LiteralText msg5 = new LiteralText("§d§l/dfgive");
-//                        LiteralText msg6 = new LiteralText("§5 version!");
-//
-//                        msg2.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + formatted)));
-//                        msg3.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + formatted)));
-//                        msg5.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + "/dfgive " + Registry.ITEM.getId(item.getItem()) + unformatted + " 1")));
-//
-//                        this.sendMessage(mc, msg1.append(msg2).append(msg3).append(msg4).append(msg5).append(msg6));
-//
-//                    } else {
-//                        ChatUtil.sendMessage("No NBT data found!", ChatType.FAIL);
-//                    }
-//                    return 1;
-//                })
-//        );
+        cd.register(ArgBuilder.literal("itemdata")
+                .executes(ctx -> {
+                    ItemStack item = mc.player.getMainHandStack();
+                    NbtCompound nbt = item.getNbt();
+                    if (nbt != null) {
+                        ChatUtil.sendMessage(String.format("§5----------§dItem Data for %s§5----------", item.getName().getString()));
+                        NbtTextFormatter formatter = new NbtTextFormatter("  ",0);
+
+                        mc.player.sendMessage(formatter.apply(nbt), false);
+
+                        String formatted = formatter.apply(nbt).getString();
+                        String unformatted = nbt.toString();
+
+                        LiteralText msg1 = new LiteralText("§5Click here to copy a ");
+                        LiteralText msg2 = new LiteralText("§d§lFormatted§5, ");
+                        LiteralText msg3 = new LiteralText("§d§lUnformatted");
+                        LiteralText msg4 = new LiteralText("§5 or ");
+                        LiteralText msg5 = new LiteralText("§d§l/dfgive");
+                        LiteralText msg6 = new LiteralText("§5 version!");
+
+                        msg2.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + formatted)));
+                        msg3.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + unformatted)));
+                        msg5.styled((style) -> style.withClickEvent(new ClickEvent(Action.RUN_COMMAND, "/copytxt " + "/dfgive " + Registry.ITEM.getId(item.getItem()) + unformatted + " " + item.getCount())));
+
+                        this.sendMessage(mc, msg1.append(msg2).append(msg3).append(msg4).append(msg5).append(msg6));
+
+                    } else {
+                        ChatUtil.sendMessage("No NBT data found!", ChatType.FAIL);
+                    }
+                    return 1;
+                })
+        );
     }
 
     @Override
     public String getDescription() {
-        return "[blue]/itemdata[reset]\n"
-                + "\n"
-                + "Shows the item NBT of the item you are holding.";
+        return """
+            [blue]/itemdata[reset]
+
+            Shows the item NBT of the item you are holding.""";
     }
 
     @Override
